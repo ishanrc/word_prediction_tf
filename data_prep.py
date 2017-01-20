7d1aa08a4f3d5fbe87414d59e5d550eecbcd5ad1
Ishan Chaudhuri <ishanrc@umich.edu>
2017-01-20 08:11:10 -0800

    Extract 4-grams

--- data_prep.py
+++ data_prep.py
@@ -1,19 +1,39 @@
-def readVocab(sentences_file):
+from itertools import islice
+from collections import namedtuple
+
+def window(seq, n):
+  it = iter(seq)
+  result = tuple(islice(it, n))
+  if len(result) == n:
+    yield result
+  for e in it:
+    result = result[1:] + (e,)
+    yield result
+
+
+def readVocab(sentence_file):
   vocab = set()
-  with open(sentences_file) as f:
+  with open(sentence_file) as f:
     for l in f:
       vocab.update(l.split())
   vocab = list(vocab)
-  vocat.sort()
+  vocab.sort()
   return vocab
 
 def getOneHot(word, vocab):
   return [1 if word == v else 0 for v in vocab]
     
-def encodeData(sentences_file, vocab):
-  data = []
-  with open(sentences_file) as f:
+def getNGrams(sentence_file, n=4):
+  with open(sentence_file) as f:
     for l in f:
       l = l.split()
-      data.append(sum(map(lambda w: getOneHot(w, vocab), l), []))
-  return data
+      yield from window(l, n)
+
+def encodeData(sentence_file, vocab):
+  inputs = []
+  labels = []
+  LabeledData = namedtuple('LabeledData', ['inputs', 'labels'])
+  for g in getNGrams(sentence_file):
+    inputs.append(getOneHot(g[0], vocab) + getOneHot(g[1], vocab) + getOneHot(g[2], vocab))
+    labels.append(getOneHot(g[3], vocab))
+  return LabeledData(inputs, labels)
