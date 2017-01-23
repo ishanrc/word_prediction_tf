4893ceab4283328674941ed67193e6f7c19af613
Ishan Chaudhuri <ishanrc@umich.edu>
2017-01-23 08:06:13 -0800

    Load data into tf

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,7 @@
 from itertools import islice
 from collections import namedtuple
+import tensorflow as tf
+import numpy as np
 
 def window(seq, n):
   it = iter(seq)
@@ -37,3 +39,17 @@ def encodeData(sentence_file, vocab):
     inputs.append(getOneHot(g[0], vocab) + getOneHot(g[1], vocab) + getOneHot(g[2], vocab))
     labels.append(getOneHot(g[3], vocab))
   return LabeledData(inputs, labels)
+
+
+def build_nn(training_data, test_data):
+  vocab_size = len(training_data.labels[0])
+  x = tf.placeholder(tf.float32, shape=[None, vocab_size * 3])
+  y_ = tf.placeholder(tf.float32, shape=[None, vocab_size])
+  
+  W = tf.Variable(tf.zeros([vocab_size * 3, vocab_size]))
+  b = tf.Variable(tf.zeros([vocab_size]))
+  y = tf.matmul(x, W) + b
+  
+  sess = tf.InteractiveSession()
+  sess.run(tf.global_variables_initializer())
+  return sess.run(y, feed_dict={x: training_data.inputs, y_: training_data.labels})
